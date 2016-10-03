de87413066ae113846bc4ca6324645bf27c70870
mypc <924714558@qq.com>
2016-10-03 21:51:27 +0800

    数组遍历

---
+++ 6-5/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zqq on 10/3/16.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		String[] name={"zqq","yfx","siguoya"};
+		for (int i = 0; i < name.length; i++) {
+			System.out.println(name[i]);
+		}
+	}
+}
