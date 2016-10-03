c4236b4f8598d5e1bcdb8c0f04788ea60204f9a3
mypc <924714558@qq.com>
2016-10-03 17:23:04 +0800

    编程练习

--- 1-3/src/HelloWorld.java
+++ 1-3/src/HelloWorld.java
@@ -3,6 +3,14 @@
  */
 public class HelloWorld {
 	public static void main(String[] args){
+		/**
+		 * print与println的区别就是:
+		 * print不会换行
+		 * println会换行
+		 */
 		System.out.print("http://www.siguoya.name/");
+		System.out.print("http://www.siguoya.name/");
+		System.out.println("http://www.siguoya.name/");
+		System.out.println("http://www.siguoya.name/");
 	}
 }
---
+++ 5-1/src/Demo1.java
@@ -0,0 +1,17 @@
+/**
+ * 实现一个成绩加到60分的功能;
+ * 输入加分前成绩,加分后成绩,以及加了多少次
+ */
+public class Demo1 {
+	public static void main(String[] args){
+		int score=50;
+		int count=0;
+		System.out.println("加分前成绩为"+score);
+		while (score<60){
+			score+=1;
+			count+=1;
+		}
+		System.out.println("加分后成绩为"+score);
+		System.out.println("共加了"+count+"次");
+	}
+}
