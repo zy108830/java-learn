376e70ab75dac949e7c70c7c38ad4e2277b25067
mypc <924714558@qq.com>
2016-10-03 12:02:05 +0800

    运算符

---
+++ 3-1/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zqq on 10/3/16.
+ */
+public class HelloWorld {
+	public static void main(String[] args){
+		int a=12;
+		float b=9.99f;
+		int c=a+(int)b;
+		System.out.println(c);
+	}
+}
