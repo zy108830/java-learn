7a311004b611bd0c39dd678fe631cd9e76b0367c
mypc <924714558@qq.com>
2016-10-04 22:29:09 +0800

    实现继承

---
+++ src/pers/imooc/second/demo3_1/Dog.java
@@ -0,0 +1,11 @@
+package pers.imooc.second.demo3_1;
+
+/**
+ * Created by zqq on 10/4/16.
+ */
+public class Dog extends Animal{
+	public static void main(String[] args) {
+		Dog dog=new Dog();
+		dog.printName();
+	}
+}
