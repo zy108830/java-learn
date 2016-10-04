4b751c427a7ae368ccf99fdfca2e7879fc3b4b1d
mypc <924714558@qq.com>
2016-10-04 11:25:05 +0800

    在类中定义方法

---
+++ 7-1/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zqq on 10/4/16.
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		HelloWorld hw=new HelloWorld();
+		hw.print();
+	}
+
+	public void print(){
+		System.out.println("Hello World");
+	}
+}
