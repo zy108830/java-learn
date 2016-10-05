c3125f7f61c33559fa6e229b9a6532cd3e19311c
mypc <924714558@qq.com>
2016-10-05 13:39:08 +0800

    Java类之Object

---
+++ src/pers/imooc/second/demo3_8/Animal.java
@@ -0,0 +1,19 @@
+package pers.imooc.second.demo3_8;
+
+/**
+ * Created by zqq on 10/4/16.
+ */
+public class Animal {
+	public String name="肥妹";
+	protected int age=23;
+	private char sex='男';
+
+	public static void main(String[] args) {
+		System.out.println("动物父类");
+	}
+
+	public void printName(){
+		System.out.println("调用父类方法输出："+this.name);
+	}
+
+}
---
+++ src/pers/imooc/second/demo3_8/Dog.java
@@ -0,0 +1,37 @@
+package pers.imooc.second.demo3_8;
+
+/**
+ * Created by zqq on 10/4/16.
+ */
+public class Dog extends Animal {
+
+    public String name="feimei";
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.printName();
+        System.out.println(dog.toString());
+    }
+
+    /**
+     * 重写父类方法
+     */
+    public void printName() {
+        //调用父类方法
+		super.printName();
+        System.out.println("调用子类方法输出: " + this.name);
+    }
+
+    /**
+     * 任何对象最终继承的的都是Object类
+     * Object类的toString()方法默认会输出当前对象的内存地址
+     * 但我们可以覆盖重写该方法
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
