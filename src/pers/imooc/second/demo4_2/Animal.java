e85502a36658e9bbc708e75f320b2c3db6227a11
mypc <924714558@qq.com>
2016-10-31 11:08:41 +0800

    类型转换

---
+++ src/pers/imooc/second/demo4_2/Animal.java
@@ -0,0 +1,15 @@
+package pers.imooc.second.demo4_2;
+
+/**
+ * Created by zqq on 10/4/16.
+ */
+public class Animal {
+
+	public static void main(String[] args) {
+
+	}
+
+	public String eat(){
+		return  "动物具有吃的能力";
+	}
+}
---
+++ src/pers/imooc/second/demo4_2/Cat.java
@@ -0,0 +1,11 @@
+package pers.imooc.second.demo4_2;
+
+/**
+ * Created by zqq on 10/4/16.
+ */
+public class Cat extends Animal {
+
+	public static void main(String[] args) {
+
+	}
+}
